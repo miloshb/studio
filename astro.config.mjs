009546d1dbcaa76ddfeaf398e58ddff0ab5844fb
miloshb <32203070+miloshb@users.noreply.github.com>
2026-06-14 17:55:53 +0200

    add i18n to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // Requires SSR mode to read live headers on request
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+  }
+});
