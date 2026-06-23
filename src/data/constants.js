f6348de7556463c037c4f0aad6fce2080c76078e
miloshb <32203070+miloshb@users.noreply.github.com>
2026-06-23 12:49:42 +0200

    standard storage constants

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,3 @@
 // src/data/constants.js
-export const LANG_STORE_KEY = "studio_lang";
-export const TZ_STORE_KEY = "studio_user_timezone";
+export const LANG_STORE_KEY = "preferredLanguage";
+export const TZ_STORE_KEY = "preferredTimezone";
